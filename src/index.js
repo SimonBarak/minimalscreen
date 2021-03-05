104b01bb1ca3502492299cf7df63d96dac39838b
Šimon Bařák <barak.simon.as@gmail.com>
2021-03-05 18:54:46 +0100

    update-4

--- src/index.js
+++ src/index.js
@@ -47,10 +47,7 @@ const insertImage = (parrentEl, imageFile) => {
 
     console.log(img);
     parrentEl.appendChild(img);
-
-    // imageInputEl.classList.add("hidden");
-    // mockupEl.classList.remove("initial-width");
-    // mockupEl.classList.remove("initial-height");
+    parrentEl.classList.remove("initial-width");
   };
   reader.readAsDataURL(imageFile);
 };
@@ -66,6 +63,53 @@ allmockupsEl.forEach((parrentEl) => {
     const { files } = event.target;
     const imageFile = files[0];
     labelEl.classList.add("hidden");
+
     insertImage(imageWrapperEl, imageFile);
   });
 });
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "-handler")) {
+    // if present, the handler is where you move the DIV from:
+    document.getElementById(elmnt.id + "-handler").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+dragElement(document.getElementById("draggable"));
